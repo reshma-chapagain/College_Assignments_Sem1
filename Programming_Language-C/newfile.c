40aba12cc23973381ffefefbfdda25a970581b7a
reshma-chapagain <reshmachapagain13@gmail.com>
2026-01-08 12:36:33 +0545

    assignments

---
+++ Programming_Language-C/newfile.c
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+int main () {
+    char a;
+    int age;
+    float tax;
+    double lightyear;
+    printf("Size of char : %lu\n " , sizeof(a));
+    printf("Size of int : %lu\n " , sizeof(age));
+    printf("Size of float : %lu\n " , sizeof(tax));
+    printf("Size of double : %lu\n " , sizeof(lightyear));
+    return 0;
+}*
+/*wap to assign two values to variable and print max using tenary operator*/
+#include<stdio.h>
+int main() {
+    int A = 100;
+    int B = 20;
+    int max;
+    max = (A>B) ? A : B;
+    printf("Max value = %d \n", max);
+    return 0;
+} 
+#include<stdio.h>
+int main() {
+    int A= 10, B=5, C;
+}
