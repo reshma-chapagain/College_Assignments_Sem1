f6571502b9a26f3625b791ada778e9815a6004f4
reshma-chapagain <reshmachapagain13@gmail.com>
2026-01-23 21:46:00 +0545

    New assignment

---
+++ Python/laptopsbuy.py
@@ -0,0 +1,5 @@
+#show message welcome blah blah then 1.dell rs 20000 2. toshiba rs 30000 3.hp rs 25000 then select option after selecting enter quantity might be multiple quantities 
+#then delivery options 1. home 1000 2. pickup free
+#then packing 1. plastic bag rs 1000 2. bag rs 2000 3. gift box rs 5000 4. none
+
+print("Welcome to Laptop Nepal")
