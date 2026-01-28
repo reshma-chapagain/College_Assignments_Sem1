1951bd0c47242e7989ee06eb35091ad8d974cb5b
reshma-chapagain <reshmachapagain13@gmail.com>
2026-01-28 11:10:54 +0545

    Assignments

---
+++ Programming_Language-C/20to2.c
@@ -0,0 +1,11 @@
+//Wap to print 20,18,16....2
+#include<stdio.h>
+int main()
+{
+    int a = 20;
+    while(a>=2);{
+        print("%d", a);
+        a-=2;
+    }
+    return 0;
+}
---
+++ Programming_Language-C/2to20usingwhile.c
@@ -0,0 +1,14 @@
+//Wap to print 2,4,6,8.....20 using while loop
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    while ( i<=20);
+    {
+        printf("%d", i);
+        i+=2;
+    }
+    return 0;
+
+}
+
