603051937bf561ea04c57a1383c4ee7f8e0435aa
reshma-chapagain <reshmachapagain13@gmail.com>
2026-02-03 12:59:45 +0545

    python

--- Python/MY_APP/main.py
+++ Python/MY_APP/main.py
@@ -10,6 +10,18 @@ def home():
 def about(): 
     return render_template("about.html")
 
+@app.route('/contact')
+def contact(): 
+    return render_template("contact.html")
+
+@app.route('/News')
+def news(): 
+    return render_template("news.html")
+
+@app.route('/Images')
+def images(): 
+    return render_template("image.html")
+
 
 
 
