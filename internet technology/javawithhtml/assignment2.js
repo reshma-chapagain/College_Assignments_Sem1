061a6f4d16a9c364818e378aeee30e2501811c48
reshma-chapagain <reshmachapagain13@gmail.com>
2026-02-05 12:05:38 +0545

    New assignments

--- internet technology/javawithhtml/assignment2.js
+++ internet technology/javawithhtml/assignment2.js
@@ -1,42 +1,39 @@
-function separatePrimeAndComposite(numberArray) {
-   
-    let result = {
-        prime: [],
-        composite: []
-    };
-
-    
-    for (let i = 0; i < numberArray.length; i++) {
-        let num = numberArray[i];
-
+function separatePrimesAndComposites(numbers) {
+  
+    function isPrime(num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 === 0 || num % 3 === 0) return false;
         
-        if (num <= 1) continue;
-
-        let isPrime = true; 
-
-        
-        for (let j = 2; j < num; j++) {
-            
-            if (num % j === 0) {
-                isPrime = false;
-                break; 
-            }
-        }
-
        
-        if (isPrime) {
-            
-            result.prime.push(num);
-        } else {
-            
-            result.composite.push(num);
+        for (let i = 5; i * i <= num; i += 6) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
         }
+        return true;
     }
 
+    const prime = [];
+    const composite = [];
     
-    return result;
+    
+    for (let num of numbers) {
+        if (num <= 1) {
+            
+            continue;
+        } else if (isPrime(num)) {
+            prime.push(num);
+        } else {
+            composite.push(num);
+        }
+    }
+    
+    return {
+        prime: prime,
+        composite: composite
+    };
 }
 
 
-const numbers = [15-24];
-console.log(separatePrimeAndComposite(numbers));
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const result = separatePrimesAndComposites(numbers);
+console.log(result);
